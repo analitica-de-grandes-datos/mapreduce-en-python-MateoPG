c066fcd10a0d04513241793f97b51bfdf48ae6f1
Mateo Pachón González <mpachong@unal.edu.co>
2022-06-11 18:21:43 -0500

    First version
    mapper and reducer first version

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,3 +1,10 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+    for line in sys.stdin:
+        column_separated = line.split(',')
+
+        sys.stdout.write("{}\t1\n".format(column_separated[2]))
